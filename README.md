375df66a5ae58b8e55aa23515f15e32124778017
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2022-11-16 20:03:38 +0100

    afegides funcions cosinus i sinus

---
+++ README.md
@@ -0,0 +1,7 @@
+# CALCULATOR
+
+In this project I used HTML, CSS and javascript to make a replica of the popular *Casio fx-82SX* calculator. It has the design and the behaviour as close as to the original for the simplest functions: $*, /, +, -$
+
+# PROGRAMMING DECISIONS
+
+For each button there is a diferent `<div></div>` tag. This makes it possible to give the element atrributes such as id, class and onclick functions.
--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-//variable global:
-console.log("a lo global");
+
+//LES GLOBALS ES LLEGEIXEN NOMÉS UN COP, EN CARREGAR LA PÀGINA
 var string_nombre= '';
 var string_nombre_operacio = '';
 var esborra_pantalla = 0;
 var arr_nombres = []; //guardo per ordre els nombres introduits
 var arr_operadors = []; //guardo per ordre els operadors
 var encesa = 0;
-//introdueix un nombre a l'objecte window, per
-//encadelar-lo com a string a dins window.string_nre.
-//passo l'id del nombre per obtenri el boto per getElementById
 
-//post: window.string_nombre conte l'string de l'ultim +nre introduit.
+//PRE: passo com a paràmetrel'id del botó que conté un número.
+//POST: window.string_nombre ara contindrà l'string de l'ultim nre introduit.
 function posa_nombre(id_clicat) {
-    var boto = document.getElementById(id_clicat);
-    var string_nombre = boto.value;
-    window.string_nombre += string_nombre;
+    var boto = document.getElementById(id_clicat); //obtinc el botó.
+    var string_nombre = boto.value;                //obtinc el número en variable entera!
+    window.string_nombre += string_nombre;         //introdueixo un nombre fet string a l'objecte window, per encadelar-lo com a string a dins window.string_nre.
     document.getElementById('pantalleta').innerHTML = window.string_nombre; 
 }
 
-//afegeixo els operadors i els nombres a les seves respectives
-//arrays Que deespres usara la funcio calcula.
-function posa_operador(id_clicat) {
+//FUNCIONAMENT: afegeix els operadors i els nombres a les seves respectives arrays ("arr_nombres" i "arr_operadors")
+//              que després usarà la funció calcula. Imprimeix per pantalla les variacions
+function posa_operador_basic(id_clicat) {
     if (window.string_nombre.length > 0) {
         var boto = document.getElementById(id_clicat);
         var string_operador = boto.value; //+,-,x,/
         window.arr_nombres.push(window.string_nombre);  //empleno l'array del nombre
         window.arr_operadors.push(string_operador);
+        
+        //////////////////////////////
+        //
+        //  SECCIO ESBORRABLE, NO DEIXEN DE FUNCIONAR BÉ ELS OPERADORS SENZILLS. NOMÉS DEIXEN DE FUNCIONAR ELS OPERADORS COMPLEXOS
+        //
+        //enretiro l'ultim element de window.arr_nombres, sota el suposit que hi hagi un nombre mes
+        // que no pas operador (cas en que previament he demanat una operacio complexa rollo sinus, cosinus
+        // codi spaggetti... aixo es una modificació que prové del malfuncionament de la funcio posa_operador_complex
+        if (arr_nombres.length - arr_operadors.length == 1) {
+            arr_nombres.pop();
+        }
+        //////////////////////////////////
         window.string_nombre = ''; //buido string nombre, per evitar introduir operadors quan no hi ha nombres a l'entrada ultima
     }
     console.log(window.arr_nombres);
@@ -38,6 +48,7 @@ function posa_operador(id_clicat) {
 //funcion calcula_operacio()
 function calcula_operacio(){
     //no hi ha nombre introduit o nomes hi ha un nombre
+    console.log(window.arr_nombres.length)
     if (window.arr_nombres.length === 0) {
         return;
     }
@@ -61,7 +72,34 @@ function calcula_operacio(){
     window.arr_operadors = [];
     window.arr_nombres = [];
 }
-       
+
+
+function posa_operador_complex(id_clicat) {
+    //les defineixo en una funció a part perquè aqui no necessitem dos nombres per fer l'operació, només un (sinus, cosinus, tangent...)
+    //if (window.arr_nombres.length === 0) {
+    //    return;
+    //}
+    var boto = document.getElementById(id_clicat);
+    var string_operador = boto.value; //sinus,cosinus,tangent...
+    if (id_clicat == "sinus") {
+        console.log("sinus clicat");
+        var a = Math.sin(window.string_nombre*Math.PI/180);
+    }
+    else if (id_clicat == "cosinus") {
+        console.log("cosinus clicat");
+        var a = Math.cos(window.string_nombre*Math.PI/180);        
+    }
+
+    document.getElementById('pantalleta').innerHTML = a;
+    window.arr_nombres.push(a);
+   
+    console.log(window.arr_nombres);
+    console.log(window.arr_operadors);
+  
+    
+}
+
+
 function reinicialitza() {
     string_nombre = '';
     string_nombre_operacio = '';
@@ -70,6 +108,8 @@ function reinicialitza() {
     arr_operadors = []; //guardo per ordre els operadors
     encesa = 0;
 }
+
+//apagar reinicialitza la funció
 function apaga(){
     document.getElementById('pantalleta').innerHTML = "&nbsp";
     console.log("Calculadora apagada i dades reinicialitzades");
@@ -80,4 +120,5 @@ function encen(){
     reinicialitza();
     posa_nombre("zero");
     reinicialitza();
+    console.log("Calculadora encesa i dades reinicialitzades (abans i despres d'apretar el boto)");
 }
